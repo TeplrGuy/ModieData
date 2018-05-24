0e0544ce6a3a3e9f58f9226ad8911bce6ac368b4
Gil <Gil@DESKTOP-HKERFC6>
2018-05-24 12:23:22 -0400

    Format Code

--- src/MovieDataInterface.java
+++ src/MovieDataInterface.java
@@ -5,26 +5,27 @@ public interface MovieDataInterface {
 
 	// GetConnection method establishes connection to our sql database
 	Connection GetConnection() throws IOException;
-	
+
 	// GetData method gets data via api call from the movie db
 	// using a string the user inputs
 	void GetData(String SearchKey) throws IOException;
-	
+
 	// GetCredits method gets data via api call from the movie db
 	// using a movie id
 	void GetCredits(String MovieID) throws IOException;
-	 
+
 	// ParseMovieData method parses json response from GetData method
 	void ParseMovieData(String Key);
-	
+
 	// ParseCreditsData method parses json response from GetCredits method
 	void ParseCreditsData(String Output);
-	
-	// AddMovie method Inserts movie into database - movie table 
+
+	// AddMovie method Inserts movie into database - movie table
 	// and updates if data already exits
-	void AddMovie(String Title, String Description, String OriginalTitle, String DirectorList, String Link, int Key, int Index);
-	
-	// AddDirector method Inserts director into database - director table 
+	void AddMovie(String Title, String Description, String OriginalTitle, String DirectorList, String Link, int Key,
+			int Index);
+
+	// AddDirector method Inserts director into database - director table
 	// and updates if data already exits
 	void AddDirector(String Name, String Link, int DirectorId);
 }
--- src/MovieDataTester.java
+++ src/MovieDataTester.java
@@ -6,65 +6,59 @@ public class MovieDataTester {
 	static MovieData md = new MovieData();
 
 	private static Scanner in;
-    private static int userInput;
-    
-    private static void options(){
-       System.out.println("Kindly follow the options below!");
-       System.out.println("1. Search For a Movie");
-       System.out.println("2. Display Options");
-       System.out.println("3. To Quit");
+	private static int userInput;
 
-   }
+	private static void options() {
+		System.out.println("Kindly follow the options below!");
+		System.out.println("1. Search For a Movie");
+		System.out.println("2. Display Options");
+		System.out.println("3. To Quit");
+	}
+
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		String search = "love";
 		md.GetConnection();
-		//System.out.println(md.MovieList.get(0).getListofDirectors().get(0).getLink());
-		
-		 
-		
-		
-		
-		 in = new Scanner(System.in);
-	        options();
-	        while(userInput != 8) {
-	            System.out.print("Main Menu: Enter a number (Enter 3 to see all options): ");
-	            String temp = in.next();
-	            try {
-	            userInput = Integer.parseInt(temp);
-	            } catch(Exception e) {
-	                userInput = 0;
-	            }
-	            try {
-	                switch(userInput){
-	                    case 1:
-	                        // List Inventory
-	                    	System.out.println("Search (Example: Spider Man): ");
-	           	            search = in.next();
-	                    	md.GetData(search);
-	                    	md.MovieList.clear();
-	               		 	
-	                        break ;
-	                    case 2:
-	                        // Sign in a part
-	                    	System.out.println();
-	                        options();
-	                        break;
-	                    case 3:
-	                    	System.out.println("Exiting...");
-	                        System.exit(0);
-	                        break;
-	                    default:
-	                        System.out.println("Invalid choice. Make sure you enter a number between 1-7");
-	                }
-	            } catch (Exception e) {
-	                // TODO: handle exception
-	                System.out.println(e.getMessage());
-	            }
-	        }
-	    }
-	
+		// System.out.println(md.MovieList.get(0).getListofDirectors().get(0).getLink());
 
-	
-	
+		in = new Scanner(System.in);
+		options();
+		while (userInput != 8) {
+			System.out.print("Main Menu: Enter a number (Enter 3 to see all options): ");
+			String temp = in.next();
+			try {
+				userInput = Integer.parseInt(temp);
+			} catch (Exception e) {
+				userInput = 0;
+			}
+			try {
+				switch (userInput) {
+				case 1:
+					// Search movie
+					System.out.println("Search (Example: Spider Man): ");
+					search = in.next();
+					md.GetData(search);
+					md.MovieList.clear();
+
+					break;
+				case 2:
+					// Display options again
+					System.out.println();
+					options();
+					break;
+				case 3:
+					// Exit
+					System.out.println("Exiting...");
+					System.exit(0);
+					break;
+				default:
+					System.out.println("Invalid choice. Make sure you enter a number between 1-7");
+				}
+			} catch (Exception e) {
+				// TODO: handle exception
+				System.out.println(e.getMessage());
+			}
+		}
+	}
+
 }
